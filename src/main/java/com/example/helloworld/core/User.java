74c057c7e76e17feb5c47e7464c8d42b6270c091
Hao93810 <jingshihao0810@gmail.com>
2015-10-15 23:25:38 +0800

    add User.java

---
+++ src/main/java/com/example/helloworld/core/User.java
@@ -0,0 +1,37 @@
+package com.example.helloworld.core;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+
+public class User {
+	private String id;
+
+    private String password;
+
+    public User() {
+        // Jackson deserialization
+    }
+
+    public User(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    @JsonProperty
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("password", password)
+                .toString();
+    }
+}
