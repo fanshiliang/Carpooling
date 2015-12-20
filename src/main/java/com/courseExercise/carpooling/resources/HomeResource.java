bc63e4af4bf2570cce3077c7fd73f00cbed7e6c5
fanshiliang <fanshiliang1994@icloud.com>
2015-12-20 12:22:09 +0800

    modify navigation

--- src/main/java/com/courseExercise/carpooling/resources/HomeResource.java
+++ src/main/java/com/courseExercise/carpooling/resources/HomeResource.java
@@ -17,6 +17,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
@@ -75,8 +76,8 @@ public class HomeResource {
 	@GET
 	@Path("navigation")
 	@Produces(MediaType.TEXT_HTML)
-	public View getNavigation(@Auth UserAuthorization authorization){
-		return new TestNavigationView(authorization);
+	public View getNavigation(@QueryParam("u") String username ){
+		return new TestNavigationView(new UserAuthorization(username, null));
 	}
 	
 	@POST
